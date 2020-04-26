b56609a21183d581fcb5ca6d1dea9adaf4390e2a
Anthony Talavera <anthony082017@gmail.com>
2020-04-26 16:52:08 -0500

    New

--- sentencias.sql
+++ sentencias.sql
@@ -1,1 +1,25 @@
-/*Qué tipo de identidades*/
+/*Qué tipo de identidades
+Nombre: Autores*/
+
+Nombre
+Genero
+Fecha de nacimiento
+País de Origen
+
+Columna y el tipo de dato
+
+autor_id int
+nombre varchar(25) tipo de string
+apellido varchar(25)
+genero char(1) --M o F
+fecha de nacimiento date
+pais de origen varchar(40)
+
+create table autores(
+    autor_id int,
+    nombre varchar(25),
+    apellido varchar(25),
+    genero char(1),
+    fecha_nacimiento date,
+    pais_origen varchar(40)
+);

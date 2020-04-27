0ce65f7f8e97ee34de8b47fdef3c304ca51eb5e0
Anthony Talavera <anthony082017@gmail.com>
2020-04-26 22:47:56 -0500

    The summary of this course

--- sentencias_final.sql
+++ sentencias_final.sql
@@ -6,19 +6,23 @@ create database if not exists libreria_cf;
 use libreria_cf;
 
 create table if not exists autores(
-    autor_id int,
-    nombre varchar(25),
-    apellido varchar(25),
-    genero char(1),
-    fecha_nacimiento date,
-    pais_origen varchar(40)
+    autor_id int not null,
+    nombre varchar(25) not null,
+    apellido varchar(25) not null,
+    seudonimo varchar(50) unique,
+    genero char(1) not null,
+    fecha_nacimiento date not null,
+    pais_origen varchar(40) not null
 );
 
-insert into autores (autor_id,nombre,genero,fecha_nacimiento,pais_origen)
-values  (1,'Codi','M','2018-01-01','Mexico'),
-        (1,'Codi','M','2018-01-01','Mexico'),
-        (1,'Codi','M','2018-01-01','Mexico'),
-        (1,'Codi','M','2018-01-01','Mexico'),
-        (1,'Codi','M','2018-01-01','Mexico');
+insert into autores (autor_id,nombre,apellido,genero,fecha_nacimiento,pais_origen)
+values  (1,'Codi','Codi','M','2018-01-01','Mexico'),
+        (2,'Codi','Codi','M','2018-01-01','Mexico'),
+        (3,'Codi','Codi','M','2018-01-01','Mexico'),
+        (4,'Codi','Codi','M','2018-01-01','Mexico'),
+        (5,'Codi','Codi','M','2018-01-01','Mexico');
+
+insert into autores (autor_id,nombre,apellido, seudonimo,fecha_nacimiento,genero,pais_origen)
+values(10,'Stephen Edwin','King','Richard Bachman','1947-09-27','M','USA');
 
 select*from autores;

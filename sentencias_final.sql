6e28c86901abfdc83938734f5dc51cf34dc122d9
Anthony Talavera <anthony082017@gmail.com>
2020-04-27 13:38:50 -0500

    The summary of this course

--- sentencias_final.sql
+++ sentencias_final.sql
@@ -6,23 +6,43 @@ create database if not exists libreria_cf;
 use libreria_cf;
 
 create table if not exists autores(
-    autor_id int not null,
+    autor_id int unsigned primary key auto_increment,
     nombre varchar(25) not null,
     apellido varchar(25) not null,
     seudonimo varchar(50) unique,
-    genero char(1) not null,
+    genero enum('M','F'),
     fecha_nacimiento date not null,
-    pais_origen varchar(40) not null
+    pais_origen varchar(40) not null,
+    fecha_creacion datetime default current_timestamp
 );
 
-insert into autores (autor_id,nombre,apellido,genero,fecha_nacimiento,pais_origen)
-values  (1,'Codi','Codi','M','2018-01-01','Mexico'),
-        (2,'Codi','Codi','M','2018-01-01','Mexico'),
-        (3,'Codi','Codi','M','2018-01-01','Mexico'),
-        (4,'Codi','Codi','M','2018-01-01','Mexico'),
-        (5,'Codi','Codi','M','2018-01-01','Mexico');
+create table if not exists libros(
+    libro_id integer unsigned primary key auto_increment,
+    autor_id int unsigned not null,
+    titulo varchar(50) not null,
+    descripcion varchar(50),
+    paginas integer unsigned,
+    fecha_publicacion date not null,
+    fecha_creacion datetime default current_timestamp,
+    foreign key(autor_id) references autores(autor_id)
+);
 
-insert into autores (autor_id,nombre,apellido, seudonimo,fecha_nacimiento,genero,pais_origen)
-values(10,'Stephen Edwin','King','Richard Bachman','1947-09-27','M','USA');
+insert into autores (nombre,apellido, seudonimo,fecha_nacimiento,genero,pais_origen)
+values('Stephen Edwin','King','Richard Bachman','1947-09-27','m','USA'),
+      ('Joane','Rowling','J.K Rowling','1947-09-27','F','Reino Unido');
+
+insert into libros(autor_id,titulo,fecha_publicacion)
+values(1,'Carrie','1974-01-01'),
+      (1, 'El mistero de Salmes Lot','1975-01-01'),
+      (1, 'El resplando','1977-01-01'),
+
+      (2,'Harry Potter y la Piedra Filosfal','1997-06-30'),
+      (2,'Harry Potter y la Camara Secreta','1978-07-2'),
+      (2,'Harry Potter y el Prisionero de Azkaban','1999-07-8'),
+      (2,'Harry Potter y el Caliz de Fuego','2000-03-20'),
+      (2,'Harry Potter y la Orden del Fenix','2003-06-21'),
+      (2,'Harry Potter y el Ministerio del Principe','2005-06-16'),
+      (2,'Harry Potter y las Reliquias de la muerte','2007-07-21');
 
 select*from autores;
+select*from libros;

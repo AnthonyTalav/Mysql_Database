11f405d61db87a5faa84d83b542e9e5c637d8cec
Anthony Talavera <anthony082017@gmail.com>
2020-05-01 12:12:47 -0500

    Numbers functions

---
+++ functionss/numbers.sql
@@ -0,0 +1,18 @@
+--Para obtener números ramdons
+
+SELECT RAND(); --Dará números con decimales
+
+--Para obtener números redondeados o sin decimales
+
+SELECT ROUND( RAND() * 100); --El multiplicador depende de los dígitos que quieras
+
+--Para obtener ciertos decimales
+
+SELECT TRUNCATE(1.1264165651,5); --Dado dicho número decimal, solo se tendrá 5 números decimales
+
+--Potencia
+
+SELECT POW(2,16); --Primer dato número y segundo potenciador
+
+
+

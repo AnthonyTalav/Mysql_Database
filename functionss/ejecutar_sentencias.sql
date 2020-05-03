bf1a848b8a0c11a06e9147dc33db42dad486c071
Anthony Talavera <anthony082017@gmail.com>
2020-05-02 21:08:03 -0500

    Execute a query inside a function

--- functionss/ejecutar_sentencias.sql
+++ functionss/ejecutar_sentencias.sql
@@ -7,7 +7,14 @@ BEGIN
     RETURN @paginas;
 END//
 
+CREATE FUNCTION obtener_ventas()
+RETURNS INT
+BEGIN   
+    SET @ventas = (SELECT (ROUND(RAND()*100)*6));
+    RETURN @ventas;
+END//
+
 DELIMITER ;
 
 UPDATE libros SET paginas = obtener_paginas();
-
+UPDATE libros SET ventas = obtener_ventas();
